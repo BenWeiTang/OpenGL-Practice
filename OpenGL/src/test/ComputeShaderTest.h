6555a21c1fb87aabdfd268d4d79cadf985157de5
Ben Tang <sjps30503@gmail.com>
2023-06-05 17:48:17 -0400

    Render basic ST using Compute Shader

--- OpenGL/src/test/ComputeShaderTest.h
+++ OpenGL/src/test/ComputeShaderTest.h
@@ -2,6 +2,9 @@
 #include "Test.h"
 #include "Texture.h"
 #include "ComputeShader.h"
+#include "VertexArray.h"
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 #include <memory>
 
 namespace test
@@ -21,6 +24,10 @@ namespace test
 		const unsigned int s_TextureHeight;
 
 		unsigned int m_Texture;
+		std::unique_ptr<Shader> m_ScreenQaudShader;
 		std::unique_ptr<ComputeShader> m_ComputeShader;
+		std::unique_ptr<VertexArray> m_QuadVAO;
+		std::unique_ptr<VertexBuffer> m_QuadVBO;
+		std::unique_ptr<IndexBuffer> m_QuadIBO;
 	};
 }
